62d334a889a357ecbf93e1a64ebe95900ef765b4
Karel <karel@manjaro.home>
2021-02-18 10:43:11 +0100

    adding edge detection

--- videoEditor.py
+++ videoEditor.py
@@ -5,6 +5,7 @@
 import argparse
 import cv2
 import sys
+import numpy as np
 
 
 # helper function to change what you do based on video seconds
@@ -33,16 +34,35 @@ def main():
         if ret:
             if cv2.waitKey(28) & 0xFF == ord('q'):
                 break
-
-            if between(cap,0,10):
-                # Grab your object in RGB and HSV color space.  
-                # Show binary frames with the fore-ground object in white and background in black.  
-                # If you carefully choose your object (i.e.   with  a  distinct  color  compared  to  the  rest  of  the  scene), 
-                # this  can  be  a  simple thresholding operation.  
-                # Choose a color space and try to improve your grabbing (e.g.fill  holes,  undetected  edges)  
-                # by  using  binary  morphological  operations.   
-                # Put  the  improvements in a different color
-                pass
+            
+            # Grab your object in RGB and HSV color space.  
+            # Choose a color space and try to improve your grabbing (e.g.fill  holes,  undetected  edges)  
+            # by  using  binary  morphological  operations.   
+            # Put  the  improvements in a different color
+            # if between(cap,0,10):
+            #    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+                # Show binary frames with the foreground object in white and background in black.  
+            #    ret2,frame = cv2.threshold(frame, 100, 255, cv2.THRESH_BINARY_INV) # cv.THRESH_TRUNC can make stuff disapear
+            
+            if between(cap,20,40):
+                # detect vertical edges
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+                sobel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+                frame = cv2.filter2D(frame, -1, sobel_x)
+                frame = cv2.GaussianBlur(frame,(5,5),cv2.BORDER_DEFAULT)
+                frame = cv2.cvtColor(frame,cv2.COLOR_GRAY2RGB)
+                # frame[np.where((frame==[0,0,0]).all(axis=2))] = [255,165,0]
+                frame[np.where(frame[:,:,0]>10)] = [0,255,0]
+                frame = cv2.GaussianBlur(frame,(5,5),cv2.BORDER_DEFAULT)
+            
+            if between(cap,0,20):
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                circles = cv2.HoughCircles(frame,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+                circles = np.uint16(np.around(circles))
+                for i in circles[0,:]:
+                    # draw the outer circle
+                    cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
 
             #if between(cap,0,2) or between(cap,4,5):
                 #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
